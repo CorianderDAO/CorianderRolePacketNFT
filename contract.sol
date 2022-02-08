448589561b5d415775521dd192e91ac371328659
Darren <kjchen@protonmail.ch>
2022-02-09 05:45:13 +0800

    Update 2 Feb-09, 2022

--- contract.sol
+++ contract.sol
@@ -1,35 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-import "./ERC721Tradable.sol";
-import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-// import "@openzeppelin/contracts/drafts/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract CorianderRulePacketV1 is ERC721Full {
+contract CorianderRulePacketV1 is ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _ogTokenIds;
-    Counters.Counter private _wlTokenIds;
+    Counters.Counter private _tokenIds;
+    mapping (uint256 => string) private _tokenURIs;
 
-    address public owner;
+    address public contractOwner;
     address[3] public admin;
     
-    constructor(address _proxyRegistryAddress) TradeableERC721Token("Coriander DAO ROLE Packet", "CRP", _proxyRegistryAddress) public {
-        owner = msg.sender;
+    constructor() ERC721("CorianderRolePacketV1", "CRP") {
+        contractOwner = msg.sender;
         admin = [
-            owner,
+            contractOwner,
             0x721D5A24e2526eB52B707687f404A611191d9cC9,
             0x9e4F26D923e8952Fa43540d6D203cBCD9f1E9fAB
-        ];
+        ]; 
     }
 
-    function uri_src(bool isOG) override private view returns (string memory) {
-        uint256 isOG_uint256 = uint256(isOG);
+    function uri_src(uint8 roleType) private view returns (string memory) {
         return string(
             abi.encodePacked(
                 "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/main/",
-                Strings.toString(isOG_uint256),
+                Strings.toString(roleType),
                 ".json"
             )
         );
@@ -39,39 +38,36 @@ contract CorianderRulePacketV1 is ERC721Full {
         for (uint i = 0; i < admin.length; i++) {
             if (admin[i] == msg.sender) {
                 return true;
-                throw;
             }
             return false;
         }
     }
 
-    function mintPacket(address mintTo, string memory tokenURI, bool isOG, uint8 level) public returns (uint256 memory) {
-        if (!adminVerify()) { throw; }
-        if (isOG) {
+    function mintPacket(address mintTo, string memory tokenURI, uint256 roleType) public returns (uint256) {
+        require (!adminVerify());
+        if (roleType == 0) {
             _tokenIds.increment();
             uint256 newItemId = _tokenIds.current();
             _mint(mintTo, newItemId);
             _setTokenURI(newItemId, uri_src(0));
             return newItemId;
         }
-        else if (level == 1) {
+        else if (roleType == 1) {
             _tokenIds.increment();
             uint256 newItemId = _tokenIds.current();
             _mint(mintTo, newItemId);
             _setTokenURI(newItemId, uri_src(1));
             return newItemId;
         }
-        else if (level == 2) {
+        else if (roleType == 2) {
             _tokenIds.increment();
             uint256 newItemId = _tokenIds.current();
             _mint(mintTo, newItemId);
             _setTokenURI(newItemId, uri_src(2));
             return newItemId;
         }
-    }
-
-    function resetTokenURI(uint256 tokenId, string uriLink) public {
-        if (!adminVerify()) { throw; }
-        _setTokenURI(tokenId, uriLink);
+        else {
+            return 88888;
+        }
     }
 }
