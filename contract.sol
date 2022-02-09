08fa4a9ef64de5ed83f9646c90b8a9c5989f41ad
Darren <kjchen@protonmail.ch>
2022-02-09 17:46:34 +0800

    Update JSONs and Contract

--- contract.sol
+++ contract.sol
@@ -12,15 +12,27 @@ contract CorianderRulePacketV1 is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     address public contractOwner;
-    address[3] public admin;
+    address[4] public admin;
+    struct Role {
+        string roleType;
+        uint8 roleCode;
+        uint256 roleCount;
+    }
+    Role og;
+    Role wl1;
+    Role wl2;
     
     constructor() ERC721("CorianderRolePacketV1", "CRP") {
         contractOwner = msg.sender;
         admin = [
             contractOwner,
             0x721D5A24e2526eB52B707687f404A611191d9cC9,
-            0x9e4F26D923e8952Fa43540d6D203cBCD9f1E9fAB
-        ]; 
+            0x9e4F26D923e8952Fa43540d6D203cBCD9f1E9fAB,
+            0xB457178C9091EDe6ce5b42ABE8D93F88Ef75a9b1
+        ];
+        og = Role('OG', 0, 0);
+        wl1 = Role('WL1', 1, 0);
+        wl2 = Role('WL2', 2, 0);
     }
     
     function adminVerify() private view returns (bool) {
@@ -32,20 +44,55 @@ contract CorianderRulePacketV1 is ERC721URIStorage {
         return false;
     }
 
-    function mintItem(address mintTo, uint8 roleType) public {
-        require (adminVerify(), "You are not Admin");
-        if (roleType == 0) {
-            awardItem(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/0.json");
+    function mintItem(address mintTo, uint8 roleCode) public {
+        if (roleCode == og.roleCode) {
+            require(og.roleCount<=88, "OG ROLE Packet is ALL Minted");
+            mintItemAttachOtherURI(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/0.json");
+            og.roleCount++;
         }
-        else if (roleType == 1) {
-            awardItem(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/1.json");
+        else if (roleCode == wl1.roleCode) {
+            require(og.roleCount<=800, "OG ROLE Packet is ALL Minted");
+            mintItemAttachOtherURI(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/1.json");
+            wl1.roleCount++;
         }
-        else if (roleType == 2) {
-            awardItem(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/2.json");
+        else if (roleCode == wl2.roleCode) {
+            require(og.roleCount<=1000, "OG ROLE Packet is ALL Minted");
+            mintItemAttachOtherURI(mintTo, "https://raw.githubusercontent.com/CorianderDAO/CorianderRolePacketNFT/master/2.json");
+            wl2.roleCount++;
         }
     }
 
-    function awardItem(address mintTo, string memory tokenURI) public returns (uint256) {
+    function inquireRoleCirculation(uint roleCode) view public returns (uint256) {
+        if (roleCode == og.roleCode) {
+            return og.roleCount;
+        }
+        else if (roleCode == wl1.roleCode) {
+            return wl1.roleCount;
+        }
+        else if (roleCode == wl2.roleCode) {
+            return wl2.roleCount;
+        }
+        else {
+            return 88888;
+        }
+    }
+
+    function inquireRoleType(uint roleCode) view public returns (string memory) {
+        if (roleCode == og.roleCode) {
+            return og.roleType;
+        }
+        else if (roleCode == wl1.roleCode) {
+            return wl1.roleType;
+        }
+        else if (roleCode == wl2.roleCode) {
+            return wl2.roleType;
+        }
+        else {
+            return "ROLE NOT DEFINED";
+        }
+    }
+
+    function mintItemAttachOtherURI(address mintTo, string memory tokenURI) public returns (uint256) {
         require (adminVerify(), "You are not Admin");
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
